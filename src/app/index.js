d2e1d20412171bf063336955608e258cecd5be7e
Elena Bychkova <lena.bychkova.200012@gmail.com>
2022-07-05 15:48:14 +0300

    dev:data are displayed

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,40 @@
+import React, { useState, useEffect } from "react";
+
 export const App = () => {
-    return <h1>Hello</h1>
+    
+   const [users, setUsers] = useState();
+    
+   useEffect(() => {
+      function getUsers() {
+    fetch('/api/users', {
+               method: "GET",
+               mode: 'no-cors',
+               headers: {
+                   'Content-Type': 'application/json'
+               }
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            setUsers(data)
+        })
+        .catch((error) => {
+            console.log('error', error)
+        })
+       }
+       getUsers()
+    }, []);
+    
+   return (
+    <div>
+                {users && users.map((user) => (
+                    <React.Fragment key={user.id}>
+                        <div>
+                            {user.Fullname}
+                        </div>
+                  </React.Fragment>
+                ))}
+     </div>
+    )
 }
