c106b39fbc5ebe59e79a91f9e1b376f36b53358e
Elena Bychkova <lena.bychkova.200012@gmail.com>
2022-07-07 14:04:37 +0300

    dev:pagination is ready

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import usePagination from "../../hooks/usePagination";
 import { UserType } from "../../types";
+import Pagination from "../Pagination";
 import ResizableTable from "../ResizableTable";
 import TRow from "../TRow";
 import "./style.css";
@@ -11,14 +13,26 @@ export type TableProps = {
 const Table: React.FC<TableProps> = ({ users }) => {
 
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
+    const requestSort = () => { 
+    users.reverse();
+  }
 
-  const requestSort = React.useCallback(() => { 
-    users = users.reverse();
-    console.log(users)
-  },
-    [users]
-  );
+    const {
+    firstContentIndex,
+    lastContentIndex,
+    nextPage,
+    prevPage,
+    page,
+    setPage,
+    totalPages,
+  } = usePagination({
+    contentPerPage: 10,
+    count: users.length,
+  });
+  
   return (
+    <div>
     <ResizableTable resizable={true} resizeOptions={{}}>
     <thead>
       <tr>
@@ -34,13 +48,15 @@ const Table: React.FC<TableProps> = ({ users }) => {
       </tr>
     </thead>
     <tbody>
-      {users && users.map((user) => (
+      {users && users.slice(firstContentIndex, lastContentIndex).map((user) => (
         <React.Fragment key={user.id}>
           <TRow user={user} />
         </React.Fragment>
       ))}
     </tbody>
-  </ResizableTable>
+    </ResizableTable>
+      <Pagination page={page} totalPages={totalPages} prevPage={prevPage} nextPage={nextPage} setPage={setPage} />
+      </div>
 )};
 
   export default React.memo(Table);
